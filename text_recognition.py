63f62539e8c86cf81b395fee87aa6da95bc9463d
Shaurya-coder <70806988+Shaurya-coder@users.noreply.github.com>
2021-03-09 10:17:40 +0530

    Update text_recognition.py

--- text_recognition.py
+++ text_recognition.py
@@ -6,15 +6,17 @@ azure read api
 Python 3rd Party Modules:-
 1. requests module (pip install requests)
 """
-
+import os
 import json# to convert json to python dictionary
 import time
 import requests# to make requests to the api
-ENDPOINT = "{your endpoint}/vision/v3.1/read/analyze"
-API_KEY = "your api key"
-IMAGE_PATH = "ocr_sample.png"
+
 
 def send_data(API_KEY,ENDPOINT,IMAGE_PATH):
+    """
+    Function to send data to 
+    Azure read api
+    """
     f = open(IMAGE_PATH,'rb')
     data = f.read()
     params   = {
@@ -23,21 +25,33 @@ def send_data(API_KEY,ENDPOINT,IMAGE_PATH):
     }
     headers = {'Ocp-Apim-Subscription-Key':API_KEY,
     'Content-Type': 'application/octet-stream'}
-    post_request = requests.post(ENDPOINT, data=data, headers = headers, params = params)
+    post_request = requests.post(ENDPOINT, data=data, headers=headers, params=params)
+    print(post_request)
     results = post_request.headers# getting the headers
-
     get_url = results.get('Operation-Location')
-    time.sleep(.50)# wait until the response is returned
+    # time.sleep(.25)# wait until the response is returned
     get_request = requests.get(get_url, headers=headers)
+    text = get_request.text
+    returned_json = json.loads(text)
+    check = returned_json.get("status")
+    while check!='succeeded':
+        time.sleep(.01)
+        print(1)
+        get_request = requests.get(get_url,headers=headers)
+        returned_json = json.loads(get_request.text)
+        check = returned_json.get("status")
+
 
     returned_json = get_request.text
 
     returned_dict = json.loads(returned_json)# converting the json to python dict
     return returned_dict
 
-
-
 def parse_json(returned_json):
+    """
+    Function to parse the json 
+    and find the text
+    """
     parsed_text = ""
     analyzed_res = returned_json.get("analyzeResult")
     read_res = analyzed_res.get("readResults")
@@ -48,7 +62,13 @@ def parse_json(returned_json):
         parsed_text += f"{text}\n"
     return parsed_text
 
+
+
+
 if __name__=="__main__":
+    ENDPOINT = "{your endpoint}/vision/v3.1/read/analyze"
+    API_KEY = "your api key"
+    IMAGE_PATH = "/Users/shauryakanda/Desktop/ocr_sample1.png"
     returned_data = send_data(API_KEY,ENDPOINT,IMAGE_PATH)
-    extracted_text = parse_json(returned_data)
-    print(extracted_text)
+    parsed_text = parse_json(returned_data)
+    print(parsed_text)
